5037f46c663d6c37a7a6c5237ce85845f0e3f689
Rajendra Singh Arora <rajesharora77077@gmail.com>
2025-06-20 10:34:31 +0530

    minor changes

--- index2.js
+++ index2.js
@@ -65,3 +65,65 @@ document.onkeydown = function (event) {
   else if (key === "Enter") calculateResult();
   else if (key === "Backspace") clearAll();
 };
+
+
+
+
+
+/* var a = "";
+var b = "";
+var f = 0;
+var oparator=null;
+var result=''
+
+function valueAdd(inp) {
+  if (f > 0) {
+    b += inp;
+    console.log(b);
+  } else {
+    a += inp;
+    console.log(a);
+  }
+}
+
+function setOperation(op) {
+  oparator = op;
+  f++;
+}
+
+function calculateResult(){
+  debugger
+  switch(oparator){
+    case '+':
+      result= parseInt(a)+parseInt(b)
+      break
+    case '-':
+      result= a-b
+      break
+    case '*':
+      result= a*b
+      break
+    case '/':
+      result= a/b
+      break
+    default:
+      console.log('invalid');
+      
+  }
+
+  console.log(result);
+  $('#r1').text(result)
+  
+}
+
+function clearAll() {
+  a = "";
+  b = "";
+  f = 0;
+  oparator = null;
+  result = "";
+  $('#r1').text("Result:");
+}
+
+
+ */
